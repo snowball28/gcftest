0728f4d0c5b9768f5446afbf81307d38f4288e30
snowball28 <satoshi.f08@hotmail.co.jp>
2019-01-30 13:17:36 +0900

    add

---
+++ index.js
@@ -0,0 +1,9 @@
+﻿/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.test02 = (req, res) => {
+	res.status(200).send('Hello World!!!');
+};
