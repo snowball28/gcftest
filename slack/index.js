b39d48aac24bd55697a4e30fd27db32bc87ef62c
snowball28 <47128287+snowball28@users.noreply.github.com>
2019-02-01 10:23:32 +0900

    Update index.js

--- slack/index.js
+++ slack/index.js
@@ -1,1 +1,9 @@
-console.log('test');
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.gcftest_slack = (req, res) => {		
+	res.status(200).send('gcftest_sclak');
+};
