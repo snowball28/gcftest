a86f245620289bf5907626ccee089f7b6b00261d
snowball28 <47128287+snowball28@users.noreply.github.com>
2019-02-01 11:11:06 +0900

    Update index.js

--- gcf/slack/index.js
+++ gcf/slack/index.js
@@ -5,6 +5,37 @@
  * @param {!express:Response} res HTTP response context.
  */
 exports.gcftest_slack = (req, res) => {	
-	console.log('github webhook tset');
-	res.status(200).send('gcftest_sclak!!!');
+	
+	//メッセージ設定
+	var data = JSON.stringify({"text": "github_commit"});
+
+	//オプション情報設定
+	var  options = {
+		hostname: 'hooks.slack.com',
+		port: 443,
+		path: '/services/TFKQALTNC/BFLNH8YQ5/Wd1TxMYyFAcTx7MOcsAdDMRo',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Content-Length': Buffer.byteLength(data)
+		}
+	};
+
+	//リクエスト
+	var req = https.request(options, (res) =>{
+		if(res.statusCode===200){
+			console.log("OK:"+res.statusCode);
+		}else{
+			console.log("Status Error:"+res.statusCode);
+		}
+	});
+
+	//そもそもなエラー時
+	req.on('error',(e)=>{
+		console.error(e);
+	});
+
+	//データ送信
+	req.write(data);
+	req.end();
 };
