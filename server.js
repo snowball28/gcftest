a2c11c7e213fa7af9a3208f6263b7e2750cd9f49
snowball28 <satoshi.f08@hotmail.co.jp>
2019-02-01 10:39:30 +0900

    aa

--- server.js
+++
@@ -1,4 +0,0 @@
-exports.getById = function() {
-  console.log('server_function_getById');
-  return 'server_function_getById';
-}
--- slack/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Responds to any HTTP request.
- *
- * @param {!express:Request} req HTTP request context.
- * @param {!express:Response} res HTTP response context.
- */
-exports.gcftest_slack = (req, res) => {		
-	res.status(200).send('gcftest_sclak!!');
-};
